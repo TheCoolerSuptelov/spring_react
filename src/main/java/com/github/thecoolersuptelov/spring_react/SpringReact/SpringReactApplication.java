60cbd50c5d5a0fb70ecb6ed621932e849e0001c7
Kirill Suptelov <kirill.suptelov@gmail.com>
2022-04-08 23:11:29 +0700

    new file:   frontend/generated/index.ts 	new file:   frontend/generated/vaadin-featureflags.ts 	new file:   frontend/generated/vaadin.ts 	new file:   frontend/index.html 	new file:   mvnw 	new file:   mvnw.cmd 	new file:   package-lock.json 	new file:   package.json 	new file:   pom.xml 	new file:   src/main/java/com/github/thecoolersuptelov/spring_react/SpringReact/SpringReactApplication.java 	new file:   src/main/resources/application.properties 	new file:   src/test/java/com/github/thecoolersuptelov/spring_react/SpringReact/SpringReactApplicationTests.java 	new file:   tsconfig.json 	new file:   types.d.ts

---
+++ src/main/java/com/github/thecoolersuptelov/spring_react/SpringReact/SpringReactApplication.java
@@ -0,0 +1,13 @@
+package com.github.thecoolersuptelov.spring_react.SpringReact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactApplication.class, args);
+	}
+
+}
